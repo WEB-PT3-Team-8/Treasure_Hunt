377f1b81ed73eb4c8e1c3434636ba8a61c6d3e7d
Leila <nb.leila10@gmail.com>
2019-11-13 11:21:20 -0600

    Initial commit

---
+++ treasure-hunt/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <h1>Welcome to Lambda's Treasure Hunt</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
